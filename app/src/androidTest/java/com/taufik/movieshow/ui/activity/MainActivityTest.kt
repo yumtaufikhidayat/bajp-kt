7b55157fdea915a83208de1cf2b2dea3a92edf0a
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-10-21 11:17:22 +0700

    Succesully implementing instrumentatation test on project.

--- app/src/androidTest/java/com/taufik/movieshow/ui/activity/MainActivityTest.kt
+++ app/src/androidTest/java/com/taufik/movieshow/ui/activity/MainActivityTest.kt
@@ -28,7 +28,6 @@ class MainActivityTest {
 
     @Before
     fun setUp() {
-        ActivityScenario.launch(SplashscreenActivity::class.java)
         ActivityScenario.launch(MainActivity::class.java)
         IdlingRegistry.getInstance().register(EspressoIdlingResource.idlingResource)
     }
@@ -48,6 +47,7 @@ class MainActivityTest {
     @Test
     fun loadDetailMovies() {
         onView(withId(R.id.nav_movie)).check(matches(isDisplayed())).perform(click())
+        onView(withId(R.id.rvMovie)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovie.size))
         onView(withId(R.id.rvMovie)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
         onView(withId(R.id.imgMovieBackdrop)).check(matches(isDisplayed()))
         onView(withId(R.id.imgPosterMovie)).check(matches(isDisplayed()))
@@ -63,13 +63,38 @@ class MainActivityTest {
         onView(withId(R.id.tvMovieLanguage)).check(matches(withText(dummyMovie[0].language)))
         onView(withId(R.id.tvMovieReadMore)).perform(click())
         onView(withId(R.id.rvTopMovieShow)).check(matches(isDisplayed()))
-//        onView(withId(R.id.nav_favorite)).perform(click())
+    }
+
+    @Test
+    fun loadTvShows() {
+        onView(withId(R.id.nav_tv_show)).check(matches(isDisplayed())).perform(click())
+        onView(withId(R.id.rvTvShow)).check(matches(isDisplayed())).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShows.size))
+    }
+
+    @Test
+    fun loadDetailTvShows() {
+        onView(withId(R.id.nav_tv_show)).check(matches(isDisplayed())).perform(click())
+        onView(withId(R.id.rvTvShow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShows.size))
+        onView(withId(R.id.rvTvShow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.imgTvShowBackdrop)).check(matches(isDisplayed()))
+        onView(withId(R.id.imgTvShowPoster)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvTvShowTitle)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvTvShowTitle)).check(matches(withText(dummyTvShows[0].title)))
+        onView(withId(R.id.tvTvShowReleaseDate)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvTvShowReleaseDate)).check(matches(withText(dummyTvShows[0].firstAirDate)))
+        onView(withId(R.id.tvTvShowOverview)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvTvShowOverview)).check(matches(withText(dummyTvShows[0].overview)))
+        onView(withId(R.id.tvTvShowRating)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvTvShowRating)).check(matches(withText(dummyTvShows[0].rating.toString())))
+        onView(withId(R.id.tvTvShowLanguage)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvTvShowLanguage)).check(matches(withText(dummyTvShows[0].language)))
+        onView(withId(R.id.tvTvShowLanguage)).perform(click())
+        onView(withId(R.id.rvTopMovieShow)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadFavoriteMovies() {
         onView(withId(R.id.nav_favorite)).check(matches(isDisplayed())).perform(click())
-//        onView(allOf(isDisplayed(), withId(R.id.rvFavoriteMovie), withId(R.id.rvFavoriteTvShow)))
         onView(withId(R.id.tabLayoutFavorite)).perform(selectTabAtPosition(0))
         onView(withId(R.id.rvFavoriteMovie)).check(matches(isDisplayed()))
         onView(withId(R.id.rvFavoriteMovie)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShows.size))
@@ -77,11 +102,12 @@ class MainActivityTest {
 
     @Test
     fun loadDetailFavoriteMovies() {
+        onView(withId(R.id.nav_movie)).check(matches(isDisplayed())).perform(click())
+        onView(withId(R.id.rvMovie)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.nav_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
         onView(withId(R.id.nav_favorite)).check(matches(isDisplayed())).perform(click())
         onView(withId(R.id.tabLayoutFavorite)).perform(selectTabAtPosition(0))
-        onView(withId(R.id.tabLayoutFavorite)).perform(swipeLeft())
-        onView(withId(R.id.rvFavoriteMovie)).check(matches(isDisplayed()))
-        onView(withId(R.id.rvFavoriteMovie)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovie.size))
         onView(withId(R.id.rvFavoriteMovie)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
         onView(withId(R.id.imgMovieBackdrop)).check(matches(isDisplayed()))
         onView(withId(R.id.imgPosterMovie)).check(matches(isDisplayed()))
@@ -98,36 +124,7 @@ class MainActivityTest {
         onView(withId(R.id.tvMovieReadMore)).perform(click())
         onView(withId(R.id.rvTopMovieShow)).check(matches(isDisplayed()))
         onView(withId(R.id.nav_favorite)).perform(click())
-    }
-
-    @Test
-    fun loadTvShows() {
-        onView(withId(R.id.nav_tv_show)).check(matches(isDisplayed())).perform(click())
-        onView(withId(R.id.rvTvShow)).check(matches(isDisplayed()))
-        onView(withId(R.id.rvTvShow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShows.size))
-    }
-
-    @Test
-    fun loadDetailTvShows() {
-        onView(withId(R.id.nav_tv_show)).check(matches(isDisplayed())).perform(click())
-//        onView(withId(R.id.rvTvShow)).check(matches(isDisplayed()))
-        onView(withId(R.id.rvFavoriteTvShow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShows.size))
-        onView(withId(R.id.rvTvShow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        onView(withId(R.id.imgTvShowBackdrop)).check(matches(isDisplayed()))
-        onView(withId(R.id.imgTvShowPoster)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvTvShowTitle)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvTvShowTitle)).check(matches(withText(dummyTvShows[0].title)))
-        onView(withId(R.id.tvTvShowReleaseDate)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvTvShowReleaseDate)).check(matches(withText(dummyTvShows[0].firstAirDate)))
-        onView(withId(R.id.tvTvShowOverview)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvTvShowOverview)).check(matches(withText(dummyTvShows[0].overview)))
-        onView(withId(R.id.tvTvShowRating)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvTvShowRating)).check(matches(withText(dummyTvShows[0].rating.toString())))
-        onView(withId(R.id.tvTvShowLanguage)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvTvShowLanguage)).check(matches(withText(dummyTvShows[0].language)))
-        onView(withId(R.id.tvTvShowLanguage)).perform(click())
-        onView(withId(R.id.rvTopMovieShow)).check(matches(isDisplayed()))
-        onView(withId(R.id.nav_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
     }
 
     @Test
@@ -141,11 +138,13 @@ class MainActivityTest {
 
     @Test
     fun loadDetailFavoriteTvShows() {
+        onView(withId(R.id.nav_tv_show)).check(matches(isDisplayed())).perform(click())
+        onView(withId(R.id.rvTvShow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.nav_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
         onView(withId(R.id.nav_favorite)).check(matches(isDisplayed())).perform(click())
         onView(withId(R.id.tabLayoutFavorite)).perform(selectTabAtPosition(1))
-        onView(withId(R.id.tabLayoutFavorite)).perform(swipeRight())
-        onView(withId(R.id.rvFavoriteTvShow)).check(matches(isDisplayed()))
-        onView(withId(R.id.rvFavoriteTvShow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShows.size))
+        onView(withId(R.id.tabLayoutFavorite)).perform(swipeLeft())
         onView(withId(R.id.rvFavoriteTvShow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
         onView(withId(R.id.imgTvShowBackdrop)).check(matches(isDisplayed()))
         onView(withId(R.id.imgTvShowPoster)).check(matches(isDisplayed()))
@@ -161,7 +160,8 @@ class MainActivityTest {
         onView(withId(R.id.tvTvShowLanguage)).check(matches(withText(dummyTvShows[0].language)))
         onView(withId(R.id.tvTvShowLanguage)).perform(click())
         onView(withId(R.id.rvTopMovieShow)).check(matches(isDisplayed()))
-//        onView(withId(R.id.nav_favorite)).perform(click())
+        onView(withId(R.id.nav_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
     }
 
     private fun selectTabAtPosition(tabIndex: Int): ViewAction {
