7fde5a9d7541b9b54b17ab0145911f514bb86ed7
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-10-23 08:53:40 +0700

    Adding unit test to favorite movie view model and favorite tv show view model.

---
+++ app/src/test/java/com/taufik/movieshow/ui/movie/viewmodel/FavoriteMovieViewModelTest.kt
@@ -0,0 +1,59 @@
+package com.taufik.movieshow.ui.movie.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
+import com.taufik.movieshow.data.MovieShowRepository
+import com.taufik.movieshow.data.source.local.entity.MovieEntity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import com.nhaarman.mockitokotlin2.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class FavoriteMovieViewModelTest {
+
+    private lateinit var viewModel: FavoriteMovieViewModel
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var movieShowRepository: MovieShowRepository
+
+    @Mock
+    private lateinit var observer: Observer<PagedList<MovieEntity>>
+
+    @Mock
+    private lateinit var pagedList: PagedList<MovieEntity>
+
+    @Before
+    fun setUp() {
+        viewModel = FavoriteMovieViewModel(movieShowRepository)
+    }
+
+    @Test
+    fun getFavoriteMovies() {
+        val dummyMovies = pagedList
+        `when`(dummyMovies.size).thenReturn(5)
+
+        val movies = MutableLiveData<PagedList<MovieEntity>>()
+        movies.value = dummyMovies
+        `when`(movieShowRepository.getFavoritedMovies()).thenReturn(movies)
+
+        val movieEntities = viewModel.getFavoriteMovies().value
+        verify(movieShowRepository).getFavoritedMovies()
+        assertNotNull(movieEntities)
+        assertEquals(5, movieEntities?.size)
+
+        viewModel.getFavoriteMovies().observeForever(observer)
+        verify(observer).onChanged(dummyMovies)
+    }
+}
---
+++ app/src/test/java/com/taufik/movieshow/ui/tvshow/viewmodel/FavoriteTvShowViewModelTest.kt
@@ -0,0 +1,59 @@
+package com.taufik.movieshow.ui.tvshow.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
+import com.taufik.movieshow.data.MovieShowRepository
+import com.taufik.movieshow.data.source.local.entity.TvShowEntity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import com.nhaarman.mockitokotlin2.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class FavoriteTvShowViewModelTest {
+
+    private lateinit var viewModel: FavoriteTvShowViewModel
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var movieShowRepository: MovieShowRepository
+
+    @Mock
+    private lateinit var observer: Observer<PagedList<TvShowEntity>>
+
+    @Mock
+    private lateinit var pagedList: PagedList<TvShowEntity>
+
+    @Before
+    fun setUp() {
+        viewModel = FavoriteTvShowViewModel(movieShowRepository)
+    }
+
+    @Test
+    fun getFavoriteTvShows() {
+        val dummyTvShows = pagedList
+        `when`(dummyTvShows.size).thenReturn(5)
+
+        val tvShows = MutableLiveData<PagedList<TvShowEntity>>()
+        tvShows.value = dummyTvShows
+        `when`(movieShowRepository.getFavoritedTvShows()).thenReturn(tvShows)
+
+        val tvShowEntities = viewModel.getFavoriteTvShows().value
+        verify(movieShowRepository).getFavoritedTvShows()
+        assertNotNull(tvShowEntities)
+        assertEquals(5, tvShowEntities?.size)
+
+        viewModel.getFavoriteTvShows().observeForever(observer)
+        verify(observer).onChanged(dummyTvShows)
+    }
+}
